777c7fa99ea02363b90141892121174edf6558d6
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2016-10-11 14:54:54 -0700

    skeleton of server.py and templates

--- server.py
+++ server.py
@@ -5,7 +5,8 @@ from jinja2 import StrictUndefined
 from flask import Flask, render_template, request, flash, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import connect_to_db, db, Cohort, PeriodTimeSlot, PairingPeriod, \
+                  Pairing, Student
 
 
 app = Flask(__name__)
@@ -16,6 +17,28 @@ app.secret_key = "delicious knishes"
 # Normally, if you use an undefined variable in Jinja2, it fails silently.
 # This is horrible. Fix this so that, instead, it raises an error.
 app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def index():
+    """Homepage."""
+
+    return render_template("homepage.html")
+
+
+@app.route('/cohorts')
+def list_cohorts():
+    """List cohorts in the system"""
+
+    cohorts = Cohort.query.all()
+    return render_template("cohorts.html", cohorts=cohorts)
+
+@app.route('/cohort/<frodo_cohort_id>')
+def get_cohort_summary(frodo_cohort_id):
+    """Get a summary for a particular cohort"""
+
+    cohort_data = Cohort.query.filter_by(frodo_cohort_id=frodo_cohort_id)
+    return render_template("cohort_summary.html", cohort_data=cohort_data)
 
 
 
