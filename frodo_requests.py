bda6740ba996ab835e52511babaf0be9a4772ec7
Bonnie Schulkin <bonnie@hackbrightacademy.com>
2017-01-26 15:50:33 -0800

    working frodo_requests that gets frodo info via the requests module

--- frodo_requests.py
+++ frodo_requests.py
@@ -3,20 +3,21 @@
 import requests
 import os
 from urllib import urlencode
+import csv
 
 FELLOWSHIP_URL = os.environ['FELLOWSHIP_URL']
 EXPLORER_URL = FELLOWSHIP_URL + "explorer/"
 LOGIN_URL = FELLOWSHIP_URL + "accounts/login/"
 
 FRODO_PASSWORD = os.environ['FRODO_PASS']
-FRODO_USERNAME = 'bonnie'
+FRODO_USERNAME = os.environ['FRODO_USER']
 
 # queries
 FRODO_ID_AND_LEVEL_QUERY_ID = '43'
 STUDENT_DATA_QUERY_ID = '44'
 
 
-def get_sql(cohort_id, query_id):
+def get_query_results(cohort_id, query_id):
     """ """
 
     # adapted this code from 
@@ -27,129 +28,90 @@ def get_sql(cohort_id, query_id):
         'password': FRODO_PASSWORD
     }
 
+    # construct the query for the sql explorer
     url_params = {'params': 'cohort_id:' + cohort_id}
+    params = '?' + urlencode(url_params)
+    query_url = EXPLORER_URL + query_id 
+    download_url = '/'.join([query_url, 'csv']) + params
 
-    url = EXPLORER_URL + query_id + '?' + urlencode(url_params)
 
     # Use 'with' to ensure the session context is closed after use.
-    with requests.Session() as s:
+    with requests.Session() as sess:
 
         # these lines influenced by http://stackoverflow.com/questions/13567507/passing-csrftoken-with-python-requests
 
         # gimme cookies
-        s.get(FELLOWSHIP_URL)
-        csrftoken = s.cookies['csrftoken']
+        sess.get(FELLOWSHIP_URL)
+        csrftoken = sess.cookies['csrftoken']
 
+        # login and password plus the necessary cookie
         payload = {
             'login': FRODO_USERNAME,
             'password': FRODO_PASSWORD,
             'csrfmiddlewaretoken': csrftoken
         }
 
-        # set the referrer to login page, so that it accepts the cookies
-        p = s.post(LOGIN_URL, data=payload, headers=dict(Referer=FELLOWSHIP_URL))
+        # in addition to the payload, set the referrer to login page, so that it accepts the cookies
+        login_result = sess.post(LOGIN_URL, data=payload, headers={'Referer': FELLOWSHIP_URL})
 
-        # print the html returned or something more intelligent to see if it's a successful login page.
-        print p.text
+        # get the csv results of the query
+        csv_results = sess.get(download_url)
 
-        # An authorised request.
-        r = s.get(url)
-        print r.text
+        # return a list of the data rows, first row is the "header"
+        return csv_results.text.split('\r\n')
 
-
-def log_in(driver):
-  """log in to frodo to access sensitive information"""
-
-  driver.get(FELLOWSHIP_URL)
-
-  try:
-    userbox = driver.find_element_by_id('id_login')
-    userbox.send_keys(FRODO_USERNAME)
-
-    passbox = driver.find_element_by_id('id_password')
-    passbox.send_keys(FRODO_PASSWORD)
-
-    submit_button = driver.find_element_by_css_selector('button.primaryAction')
-    submit_button.click()
-
-    return True
-
-  except NoSuchElementException:
-    # no login info. Brand new browser instance, so there's no way we're
-    # already logged in. Return False.
-    return False
-
-def get_sql_rows(driver, cohort_id, query_id, col_count):
-  """return list rows of data as a list from the frodo SQL Explorer.
-
-  Each row is represented as a sublist, with each column value as an item.
-  """
-
-  url_params = {'params': 'cohort_id:' + cohort_id}
-
-  url = EXPLORER_URL + query_id + '?' + urlencode(url_params)
-  driver.get(url)
-
-  html_text = driver.page_source.encode("utf8", "ignore")
-  soup = BeautifulSoup(html_text, 'html.parser')
-  rows = soup.find_all('tr', { 'class': 'data-row'})
-
-  # use beautiful soup to parse results out of the page -- classes are the 
-  # column number in this instance
-  query_data = []
-  for row in rows: 
-    rowdata = []
-    for i in range(col_count):
-      rowdata.append(row.find('td', {'class': str(i)}).get_text())
-    query_data.append(rowdata)
-
-  return query_data
 
 def get_students(cohort_id):
-  """get initial super data to load into pairings db"""
+    """get initial super data to load into pairings db, return dict
 
-  # new driver for login and data extraction
-  driver = webdriver.Firefox()
-  
-  student_data = {}
+    {frodo_id: 
+        {'fullname': fullname, 
+            'level': int(level)
+        }
+    }
 
-  if log_in(driver):
-    # only collect data if login was possible
+    """
 
-    rows = get_sql_rows(driver, cohort_id, STUDENT_DATA_QUERY_ID, col_count=5)
+    rows = get_query_results(cohort_id, STUDENT_DATA_QUERY_ID)
 
-    for frodo_id, first_name, nickname, last_name, level in rows:
-      if nickname:
-        first_name = nickname
+    student_data = {}
 
-      fullname = ' '.join([first_name, last_name])
-      student_data[frodo_id] = {'fullname': fullname, 'level': int(level)}
+    for row in rows[1:]: # skip header row
 
-  driver.close()
-  return student_data
+        if not row: 
+        # skip empty rows
+            continue
+
+        frodo_id, first_name, nickname, last_name, level = row.split(',')
+
+        if nickname:
+            first_name = nickname
+
+        fullname = ' '.join([first_name, last_name])
+        student_data[frodo_id] = {'fullname': fullname, 'level': int(level)}
+
+    return student_data
 
 
 def get_levels(cohort_id):
   """get student levels from frodo and return dict {frodo_id: level}
 
-  example URL: 
-  http://fellowship.hackbrightacademy.com/explorer/43/?params=cohort_id%3Af16g
   """
 
-  # new driver for login and data extraction
-  driver = webdriver.Firefox()
-  
+  rows = get_query_results(cohort_id, FRODO_ID_AND_LEVEL_QUERY_ID)
+
   student_levels = {}
 
-  if log_in(driver):
-    # only collect data if login was possible
+  # skip header row
+  for row in rows[1:]:
+    if not row: 
+      # skip empty rows
+      continue
 
-    rows = get_sql_rows(driver, cohort_id, FRODO_ID_AND_LEVEL_QUERY_ID, col_count=2)
 
-    for frodo_id, level in rows:
-      student_levels[frodo_id] = int(level)
+    frodo_id, level = row.split(',')
+    student_levels[frodo_id] = int(level)
 
-  driver.close()
   return student_levels
 
 
@@ -158,8 +120,8 @@ if __name__ == '__main__':
   # for testing
   cohort = 'f17g'
   print "*" * 8 + 'STUDENT DATA'
-  print get_sql(cohort, FRODO_ID_AND_LEVEL_QUERY_ID)
+  print get_students(cohort)
 
-  # print 
-  # print "*" * 8 + 'LEVEL DATA'
-  # print get_levels(cohort)
+  print 
+  print "*" * 8 + 'LEVEL DATA'
+  print get_levels(cohort)
